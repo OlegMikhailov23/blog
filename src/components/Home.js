8eecf5ab310ee81cc93eb0dc14cf69bd91a1300c
Oleg Mikhaylov <dkfolkin@gmail.com>
2021-02-16 20:30:13 +0300

    feat: added blog section

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,26 +4,27 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: 'Oleg',
-            age: '33',
+            blogs: [
+                {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+                {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+                {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
+            ]
         }
     }
 
     handleClick(i) {
-        this.setState({
-            name: 'Igor',
-            age: '32'
-        })
+        this.setState({})
     }
 
     render() {
         return (
             <div className="home">
-                <h2>Home page</h2>
-                <p>{ this.state.name } is { this.state.age }</p>
-                <button onClick={(e) => {
-                    this.handleClick(e);
-                }}>Click Me</button>
+                {this.state.blogs.map(blog => (
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>Write by {blog.author}</p>
+                    </div>
+                ))}
             </div>
         );
     }
