775f0c977ac9becb2cf6835828f79ef7040f4f40
Oleg Mikhaylov <dkfolkin@gmail.com>
2021-02-17 17:47:02 +0300

    feat: used useEffect in Class

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect} from 'react';
 import BlogList from "./BlogList";
+
 
 class Home extends Component {
     constructor(props) {
@@ -9,7 +10,8 @@ class Home extends Component {
                 {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
                 {title: 'Welcome party!', body: 'lorem ipsum...', author: 'oleg', id: 2},
                 {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'oleg', id: 3}
-            ]
+            ],
+            name: 'Oleg'
         }
     }
 
@@ -21,10 +23,21 @@ class Home extends Component {
         });
     }
 
+    componentDidMount() {
+        console.log('ПРивет', this.state.blogs)
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.name !== this.state.name) console.log('ПРивет', this.state.blogs)
+    }
+
     render() {
         return (
             <div className="home">
+                <p>{this.state.name}</p>
                 <BlogList blogs={this.state.blogs} title={"All Blogs"} handleDelete={this.handleDelete.bind(this)}/>
+                <button onClick={() => this.setState({
+                    name: 'Luigi',
+                })}>Change Name</button>
             </div>
         );
     }
